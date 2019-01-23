f304c2d3b58c8d53a275e424c068d17f05d9ada8
Lanh Hoang <lanh.hoangcong@gmail.com>
2019-01-23 13:56:05 +0700

    Add available products

---
+++ db/products.rb
@@ -0,0 +1,7 @@
+module Products
+  PRODUCTS = {
+    '001': { name: 'Lavender heart', price: 9.25 },
+    '002': { name: 'Personalised cufflinks', price: 45.00 },
+    '003': { name: 'Kids T-shirt', price: 19.95 }
+  }
+end
