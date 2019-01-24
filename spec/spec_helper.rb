42aa845393f00d6ec48bc2dedfead3496927d612
Lanh Hoang <lanh.hoangcong@gmail.com>
2019-01-24 13:36:36 +0700

    Modify spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+%w(app db lib).each do |dir|
+  Dir.glob(Dir.pwd + "/#{dir}/**/*.rb").each {|f| require f}
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
