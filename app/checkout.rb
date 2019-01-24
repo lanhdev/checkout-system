82f127fe0f2eb04ecfc12470c4e2f794badf18cc
Lanh Hoang <lanh.hoangcong@gmail.com>
2019-01-24 15:08:04 +0700

    Merge Checkout and Basket

--- app/checkout.rb
+++ app/checkout.rb
@@ -1,16 +1,31 @@
 class Checkout
-  def initialize(promotions)
-    @basket = Basket.new(promotions)
+  def initialize(promotional_rules)
+    @promotional_rules = promotional_rules
+    @products = []
   end
 
   def scan(code)
     product = Product.new(code)
-    basket.add_product(product)
+    products.push(product)
   end
 
   def total
-    '£%.2f' % basket.total
+    '£%.2f' % (total_price - discount)
   end
 
-  attr_reader :basket
+  private
+
+  attr_reader :promotional_rules, :products
+
+  def total_price
+    products.reduce(0) { |sum, product| sum + product.price }
+  end
+
+  def discount
+    promotions.reduce(0) { |discount, promotion| discount += promotion.apply(products) }
+  end
+
+  def promotions
+    promotional_rules.sort! { |x, y| x.discount_type <=> y.discount_type }
+  end
 end
